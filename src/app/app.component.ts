753c2ae054bb9c9e43fd82a2e4cca8a4ac5615df
Salvador Subarroca <subarroca@gmail.com>
2017-05-26 14:55:49 +0200

    add web component with angular integration and slots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  theme = '';
+  isCollapsed: boolean;
+
+  toggleCollapsed(isCollapsed: boolean) {
+    this.isCollapsed = isCollapsed;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -8,6 +8,9 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   imports: [
     BrowserModule,
